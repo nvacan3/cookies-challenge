054fcbb9bf8d1deb33ac4ba2862156cb1351518c
nvacan3 <nicolas.b.moise@gmail.com>
2023-05-22 14:36:13 -0400

    completes task

--- server.js
+++ server.js
@@ -1,34 +1,27 @@
-const express = require('express');
-const cookieParser = require('cookie-parser')
+const express = require("express");
+const cookieParser = require("cookie-parser");
+let cookieSession = require("cookie-session");
 const server = express();
-const port = 8003
+const port = 8003;
 
-server.listen(port, () => console.log(`Server is running on ${port}`))
+server.listen(port, () => console.log(`Server is running on ${port}`));
 server.use(express.json());
 server.use(cookieParser());
-server.use(cookieSession ({
-    name: 'user_session'
-}))
 
-server.get('/login', (req, res) => {
-    if (loginSuccess(req.body.username, req.body.password)) {
-        req.session.username = req.body.username
-    };
-    
-    var opts = {
-        httpOnly: true, 
-        sameSite: 'strict'
-    };
-});
-res.cookie('some_name', 'some_value', opts)
-
-
-// Create an Express application that sets a cookie when routed to /login with their name. 
+// Create an Express application that sets a cookie when routed to /login with their name.
 // If a cookie is present with a name key, then it says "welcome {name}! when the user routes to /hello
+server.use(
+  cookieSession({
+    name: "user_session",
+    secret: 'somesuperimpressivestringofcharactersnoonewouldpossiblyguessmaybe',
+  })
+);
 
-server.post('/login', (req, res, next) => {
-    if (loginSuccess(req.body.username, req.body.password)) {
-        req.session.username = req.body.username;
-    }
+server.get("/login", (req, res) => {
+  res.cookie("name", "Tobi");
+  res.send();
+});
+
+server.get('/hello', (req, res) => {
+    res.send(`welcome ${req.cookies.name}!`);
 })
-
