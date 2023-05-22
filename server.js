f8fd1e2e3d8e6d8c75c9ce6d63c7b2ef3701bf2a
nvacan3 <nicolas.b.moise@gmail.com>
2023-05-22 12:46:31 -0400

    adds res.cookie

--- server.js
+++ server.js
@@ -3,11 +3,32 @@ const cookieParser = require('cookie-parser')
 const server = express();
 const port = 8003
 
+server.listen(port, () => console.log(`Server is running on ${port}`))
 server.use(express.json());
 server.use(cookieParser());
+server.use(cookieSession ({
+    name: 'user_session'
+}))
 
-server.get('/', (req, res) => {
-    res.status(200).send('Welcome')
+server.get('/login', (req, res) => {
+    if (loginSuccess(req.body.username, req.body.password)) {
+        req.session.username = req.body.username
+    };
+    
+    var opts = {
+        httpOnly: true, 
+        sameSite: 'strict'
+    };
+});
+res.cookie('some_name', 'some_value', opts)
+
+
+// Create an Express application that sets a cookie when routed to /login with their name. 
+// If a cookie is present with a name key, then it says "welcome {name}! when the user routes to /hello
+
+server.post('/login', (req, res, next) => {
+    if (loginSuccess(req.body.username, req.body.password)) {
+        req.session.username = req.body.username;
+    }
 })
 
-server.listen(port, () => console.log(`Server is running on ${port}`))
